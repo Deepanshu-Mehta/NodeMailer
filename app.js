a093c2b8893a4619e9cec511f6d9104e88c888c4
Deepanshu-Mehta <146458164+Deepanshu-Mehta@users.noreply.github.com>
2024-11-05 18:04:59 +0530

    updated

--- app.js
+++ app.js
@@ -2,12 +2,7 @@ const express = require('express');
 const sendEmail = require('./controller/sendEmail');
 
 const app = express();
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type');
-//     res.header('Access-Control-Allow-Methods', 'POST');
-//     next();
-//   });
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
